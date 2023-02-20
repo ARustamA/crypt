5341e918710dbb0b74dd06f210d65fedca285e39
“ARustamA” <rakhmetyanov@gmail.com>
2023-02-20 13:04:36 +0600

    d

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,28 @@ import { PrivateRoute } from './utils/router/PrivateRoute';
 
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { AuthRootComponent } from './components/auth';
+import { ColorModeContext, useMode } from './theme';
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import './index.css';
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <Router>
-      <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route path="/login" element={<AuthRootComponent />} />
-        <Route path="/register" element={<AuthRootComponent />} />
-      </Routes>
-    </Router>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+            <Route path="/login" element={<AuthRootComponent />} />
+            <Route path="/register" element={<AuthRootComponent />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
